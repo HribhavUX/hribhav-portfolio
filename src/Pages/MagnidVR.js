fddc9c8332333c61416eac76f5a0172a910a232e
Ramanjs <raman.s2662@gmail.com>
2023-02-06 02:58:50 +0530

    add: magnidvr landing

--- src/Pages/MagnidVR.js
+++ src/Pages/MagnidVR.js
@@ -1,66 +1,36 @@
-import MagnidVR_background1 from "../Assets/MagnidVR_background1.png";
-import MagnidVR_background2 from "../Assets/MagnidVR_background2.png";
-import MagnidVR_background3 from "../Assets/MagnidVR_background3.png";
-import MagnidVR_background4 from "../Assets/MagnidVR_background4.png";
 import Section1 from "../Components/MagnidVR/Section1";
 import Section2 from "../Components/MagnidVR/Section2";
+import hero from "../Assets/MagnidVR/hero.png";
+import bg from "../Assets/MagnidVR/bg.png";
 
-export default function MagnidVR(props) {
-   return(
-     <>
-       <div className='w-full flex justify-center px-36 h-full max-h-screen'>
-           <div className='absolute w-full z-0 top-0 max-h-full overflow-hidden'>
-             <img src={MagnidVR_background1} className='w-full' alt='magnidVR'/>
-           </div>
-           <div className='w-6/12 pl-48 flex flex-col justify-center z-30 pt-[4.5rem]'>
-             <div className='w-full'>
-               <p className='text-white w-full font-Sgb text-[2.65rem] pt-20'>Magnid VR</p>
-                 <p className='text-white w-full font-Sgr text-sm pt-0'>
-                        Helps organizations plan and host events using VR technology to enhance the event experience for attendees and make it more interactive, engaging, and immersive.
-                 </p>
-                 <p className='text-white w-1/2 font-PoB text-base pt-10 pb-1.5'>My Role</p>
-                 <p className='text-sm text-white w-1/2 font-PoR'>UX Designer | Unity Developer</p>
-                 <div className='flex w-full pt-14 justify-start pr-[0rem] flex-wrap'>
-                   <div className='font-PoM flex z-20 bg-[#6900BB] min-w-fit text-white text-sm px-4 mr-5 py-1 my-1 rounded-xl items-center justify-center'>
-                            Virtual Reality
-                   </div>
-                   <div className='font-PoM flex z-20 bg-[#6900BB] min-w-fit text-white text-sm px-6 mr-5 py-1 my-1  rounded-xl items-center justify-center'>
-                            Metaverse
-                   </div>
-                   <div className='font-PoM flex z-20 bg-[#E7018B] min-w-fit text-white text-sm px-7 py-1 my-1 rounded-xl items-center justify-center'>
-                            Miro
-                   </div>
-                   </div>
-                   <div className='flex w-full py-1 justify-start flex-wrap'>
-                     <div className='font-PoM flex z-20 my-1 bg-[#6900BB] min-w-fit text-white text-sm px-5 mr-5 py-1 rounded-xl items-center justify-center'>
-                            Event Curation
-                     </div>
-                     <div className='font-PoM flex z-20 my-1 bg-[#E7018B] min-w-fit text-white text-sm px-5 mr-5 py-1 rounded-xl items-center justify-center'>
-                            Oculus
-                     </div>
-                     <div className='font-PoM flex z-20 my-1 bg-[#E7018B] min-w-fit text-white text-sm px-5 mr-5 py-1 rounded-xl items-center justify-center'>
-                            Figma
-                     </div>
-                     <div className='font-PoM flex z-20 my-1 bg-[#E7018B] min-w-fit text-white text-sm px-7 py-1 rounded-xl items-center justify-center'>
-                            Unity
-                     </div>
-                  </div>
-              </div>
+export default function MagnidVR() {
+  return(
+    <>
+      <div className="font-PoR px-36 min-h-screen flex justify-between items-center text-white">
+        <img className="absolute top-0 left-0 right-0 -z-10" src={bg} alt="background"/>
+        <div className="flex flex-col justify-around self-stretch">
+          <div>
+            <h1 className="my-2 font-PoB text-5xl italic">Magnid VR</h1>
+            <p className="my-4 max-w-xs">A seamless VR experience for virtual events in metaverse</p>
+          </div>
+          <div>
+            <div className="flex my-2">
+              <div className="mx-2 px-4 py-2 bg-[#6900BB] rounded-[10px] text-white shadow-md">Metaverse</div>
+              <div className="mx-2 px-4 py-2 bg-[#6900BB] rounded-[10px] text-white shadow-md">Virtual Reality</div>
+              <div className="mx-2 px-4 py-2 bg-[#6900BB] rounded-[10px] text-white shadow-md">Exploration</div>
             </div>
-            <div className='w-[40%] pl-0 flex flex-col justify-center items-center'>
-                <div className='absolute w-[26%] flex justify-center items-center mt-28 z-10'>
-                    <img src={MagnidVR_background2} className='w-[100%]' alt='magnid background 2'/>
-                </div>
-                <div className='absolute mr-[20vw] w-[20%] -mt-[10vw] flex justify-center items-center z-0'>
-                    <img src={MagnidVR_background3} className='w-[100%]' alt='magnid background 3'/>
-                </div>
-                <div className='absolute ml-[12.5vw] w-[16%] mt-[8vw]  flex flex-col justify-center items-center z-10'>
-                    <img src={MagnidVR_background4} className='w-[100%]' alt='magnid background 4'/>
-                </div>
+            <div className="flex my-2">
+              <div className="mx-2 px-4 py-2 bg-[#E7018B] rounded-[10px] text-white shadow-md">Illustrator</div>
+              <div className="mx-2 px-4 py-2 bg-[#E7018B] rounded-[10px] text-white shadow-md">Figma</div>
+              <div className="mx-2 px-4 py-2 bg-[#E7018B] rounded-[10px] text-white shadow-md">Unity</div>
+              <div className="mx-2 px-4 py-2 bg-[#E7018B] rounded-[10px] text-white shadow-md">Miro</div>
             </div>
+          </div>
         </div>
-        <Section1 />
-        <Section2 />
-       </>
-    )
+        <img className="w-1/2" src={hero} alt="hero"/>
+      </div>
+      <Section1 />
+      <Section2 />
+     </>
+  )
 }
