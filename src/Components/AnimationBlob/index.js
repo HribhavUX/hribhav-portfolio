3ad38b4f211acac900f93b9ae01d0f9f8f315f65
mishraanis <mishraanis@gmail.com>
2023-02-22 01:26:27 +0530

    some more changes

--- src/Components/AnimationBlob/index.js
+++ src/Components/AnimationBlob/index.js
@@ -1,4 +1,4 @@
-import animationBlob from "../../Assets/animationBlob.png";
+import animationBlob from "../../Assets/blob-bg.png";
 import AboutMe from "../AboutMe";
 
 
@@ -7,10 +7,12 @@ export default function AnimationBlob(){
     return (
         <div className='w-full flex flex-col items-center'>
             <div className='w-9/12 -mt-[5vh] flex flex justify-center items-center'>
-                <div className= "relative">
-                <img src={animationBlob} className='w-full z-0' alt='Animation Blob' />
-                {/* <div className='w-full absolute h-full '> */}
-                    {/*<iframe title='spline-3d-stuff' className='spline-comp' src='https://my.spline.design/untitledcopy-cf0fbbc37953b3ef6fa8316c5e316edc/' width='100%' height='100%'></iframe>*/}
+                <div className="relative w-full">
+                    <img src={animationBlob} className='w-full z-0 ml-[3%]' alt='Animation Blob'/>
+                    <iframe className='w-[33%] left-[26vw] absolute bottom-[10%] z-20'
+                            src='https://my.spline.design/untitledcopy-892aec6ddd4c393192eadbcb7c2ca04e/'
+                             width='100%' height='100%'></iframe>
+                    <div className='w-full right-0 absolute bottom-[12%] h-[3.5vmax] bg-[#15151A] z-30'></div>
                 </div>
             </div>
             <p className='font-Ga text-[2.65rem] flex justify-center items-center text-white w-7/12 pt-20 pb-80'>
